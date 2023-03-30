256c9bb6c0dd1e9956a50bfc751b5b45c1ae72d2
Ewelina <ewela.m95@gmail.com>
2023-03-30 20:40:02 +0200

    Create changeNavBarColor function

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,26 @@
 import { NavLink } from 'react-router-dom';
 import style from './NavBar.module.css';
 import './NavBar.css';
+import { useState } from 'react';
 
 const NavBar = () => {
+	const [colorChange, setColorchange] = useState(true);
+	
+	const changeNavbarColor = () => {
+		if (window.scrollY >= 80) {
+			setColorchange(true);
+		} else {
+			setColorchange(false);
+		}
+	};
+	window.addEventListener('scroll', changeNavbarColor);
+
 	return (
-		<nav className={style.nav}>
+		<nav
+			className={`${style.nav} ${
+				colorChange ? style.navWithBackground : style.transparent
+			}`}
+		>
 			<div className={style.logo}>
 				<NavLink to='/DentClinicApp/' title='Strona główna'>
 					<p>
